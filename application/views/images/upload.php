07a36dd45e590f7983ecfb6b64f10ce768f1b13a
afiqrasyidm <afiqrasyidm@gmail.com>
2019-04-03 15:12:22 +0700

    add upload feature

---
+++ application/views/images/upload.php
@@ -0,0 +1,9 @@
+<link rel = "stylesheet" type = "text/css" href ="<?php echo base_url('assets/css/upload.css');?>">
+<script src="upload.js"> </script>
+<div>
+  <form action="upload.php" method="POST">
+    <input type="file" multiple>
+    <p>Drag your files here or click in this area.</p>
+    <button type="submit">Upload</button>
+  </form>
+</div>
--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,6 +1,10 @@
-<div class="jumbotron text-center" style="margin-bottom:0">
-  <p>Indtagram adalah tempat kamu berbagi cerita melalui gambar</p>
-</div>
+
+
+<footer>
+    <div class="jumbotron text-center" style="margin-bottom:0">
+    <p>Indtagram adalah tempat kamu berbagi cerita melalui gambar</p>
+  </div>
+</footer>
 
 </body>
 </html>

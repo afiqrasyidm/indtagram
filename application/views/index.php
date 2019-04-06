15e447c4450998008b1073dc85b64da2aa727616
afiqrasyidm <afiqrasyidm@gmail.com>
2019-04-06 19:07:59 +0700

    ubah view

--- application/views/index.php
+++ application/views/index.php
@@ -6,24 +6,21 @@
     <div class="col-md-6 offset-md-3">
 				<br>
       <div class="img">
+        <p> Diupload oleh: afiqrasyidm@gmail.com</p> </p>
 				<a href="<?php echo site_url('gambar/detail/1'); ?>"> <img src="https://appharbor.com/assets/images/stackoverflow-logo.png" alt="..." class="img-thumbnail"> </a>
 			</div>
 			<br>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-			<hr style="height:1px; border:none; color:#000; background-color:#000;">
-			<p>Komentar:</p>
-			<p>Afiq: Wah keren kak</p>
-			<hr>
+    	<hr style="height:1px; border:none; color:#000; background-color:#000;">
+
 			<br>
-			<div class="img">
-				<img src="https://appharbor.com/assets/images/stackoverflow-logo.png" alt="..." class="img-thumbnail">
-			</div>
-			<br>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-			<hr style="height:1px; border:none; color:#000; background-color:#000;">
-			<p>Komentar:</p>
-			<p>Afiq: Wah keren kak</p>
-			<hr>
+      <div class="img">
+        <p> Diupload oleh: afiqrasyidm</p> </p>
+        <a href="<?php echo site_url('gambar/detail/1'); ?>"> <img src="https://appharbor.com/assets/images/stackoverflow-logo.png" alt="..." class="img-thumbnail"> </a>
+      </div>
+      <br>
+      <hr style="height:1px; border:none; color:#000; background-color:#000;">
+
+      <br>
     </div>
   </div>
 </div>
